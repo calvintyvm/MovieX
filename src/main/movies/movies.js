c835b68d1f01493329f7d31d9ef9dea0b1beb851
Calvin Ting <calvinting@protonmail.com>
2018-04-01 18:17:50 -0700

    added sass, did not work, added styling for app, added years and rating

--- src/main/movies/movies.js
+++ src/main/movies/movies.js
@@ -5,25 +5,33 @@ import MovieListItem from "./MovieListItem";
 const movies = ["Breaking Bad","Narcos", "Game of Thrones"];
 
 class Movies extends React.Component{
+    state= {
+        movies:[]
+    }
     componentDidMount(){
         const apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=e487d8aa73d8fceee7247c65a3379a4d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
         
         fetch(apiUrl)
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => this.storeMovies(data))
             .catch(error => console.log(error))
         console.log("Before or After the data?");
     }
-
-
+    storeMovies = data =>{
+        const movies = data.results.map(result=>{
+            const{ vote_count, id, genre_ids, poster_path, title, vote_average, release_date } = result;
+            return{ vote_count, id, genre_ids, poster_path, title, vote_average, release_date };
+        });
+        this.setState({movies})
+    }
 
     render(){
         return(
-            <section>
+            <section className="moviesContainer">
                 <ul className ="movies">
                 {
-                    movies.map((title,index)=>(
-                        <MovieListItem key={index.toString()} title={title}/>
+                    this.state.movies.map(movie =>(
+                        <MovieListItem key={movie.id} movie={movie}/>
                     ))
                 }
                 </ul>
