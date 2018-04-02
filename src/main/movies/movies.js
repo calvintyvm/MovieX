6295ed0a2c1d0d51df38c119ad979d798ad96a04
Calvin Ting <calvinting@protonmail.com>
2018-04-01 17:13:09 -0700

    added api manually in movies js

--- src/main/movies/movies.js
+++ src/main/movies/movies.js
@@ -1,11 +1,32 @@
 import React from "react";
 import "./movies.css";
+import MovieListItem from "./MovieListItem";
+
+const movies = ["Breaking Bad","Narcos", "Game of Thrones"];
 
 class Movies extends React.Component{
+    componentDidMount(){
+        const apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=e487d8aa73d8fceee7247c65a3379a4d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
+        
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+        console.log("Before or After the data?");
+    }
+
+
+
     render(){
         return(
-            <section className="movies">
-            Movies
+            <section>
+                <ul className ="movies">
+                {
+                    movies.map((title,index)=>(
+                        <MovieListItem key={index.toString()} title={title}/>
+                    ))
+                }
+                </ul>
             </section>
         )
     }
